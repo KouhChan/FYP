7bf24d3cc3382d6a49a63998fa5cf555ee84c912
SaiKou.Repo <130267866+KouhChan@users.noreply.github.com>
2024-04-25 00:56:38 +0800

    Update  on Feedback Form and Fetching Report
    Feedback Form
    - Create a Feedback form for student to report
    
    Admin Feedback Report
    - Fetching data from firebase for student report

---
+++ JS/Report.js
@@ -0,0 +1,48 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAg8iVwGi-X6dJCe15dvavK0ndAoVPutsA",
+    authDomain: "university-bus-system.firebaseapp.com",
+    databaseURL: "https://university-bus-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "university-bus-system",
+    storageBucket: "university-bus-system.appspot.com",
+    messagingSenderId: "446380655695",
+    appId: "1:446380655695:web:ee019fad4684435252163a"
+};
+
+
+
+//initialize the firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference to database
+var contactFormDB = firebase.database().ref("Report");
+
+//get form from HTML
+document.getElementById("report").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    var nama = getElementVal('name');
+    var id = getElementVal('SId');
+    var location = getElementVal('location');
+    var Date = getElementVal('Date');
+    var description = getElementVal('desc');
+
+    saveMessage(nama, id, location, Date, description);
+};
+
+const saveMessage = (nama, id, location, Date, description) => {
+    var newReport = contactFormDB.child(id);
+
+    newReport.set({
+        nama: nama,
+        ID: id,
+        Location: location,
+        Date: Date,
+        Report: description,
+    });
+};
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+};
